1278eb9784ca0544fc2ce93b76689a541ee2314d
mokasha446 <mokasha446@gmail.com>
2026-06-04 03:47:55 +0500

    Optimized Vite Configuration

--- vite.config.ts
+++ vite.config.ts
@@ -7,4 +7,31 @@ export default defineConfig({
   resolve: {
     tsconfigPaths: true,
   },
+  build: {
+    minify: "terser",
+    terserOptions: {
+      compress: {
+        drop_console: ["log"],
+        drop_debugger: true,
+      },
+    },
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            if (
+              id.includes("react-router") ||
+              id.includes("react") ||
+              id.includes("react-dom") ||
+              id.includes("@react-router")
+            ) {
+              return "vendor-react";
+            }
+            return "vendor-libs";
+          }
+        },
+      },
+    },
+    chunkSizeWarningLimit: 600,
+  },
 });
